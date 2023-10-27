d26da546030cce4bd3a0ba097773217336d74308
Kevin <ksully121888@gmail.com>
2023-10-27 14:55:15 -0400

    fixed the load on scroll and some css stuff

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-function isElementInViewport(el) {
-    var rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
+// function isElementInViewport(el) {
+//     var rect = el.getBoundingClientRect();
+//     // return (
+//     //   rect.top >= 0 &&
+//     //   rect.left >= 0 &&
+//     //   rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     //   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     // );
+//     return (
+        
+
+//     )
+//   }
   
   function handleScroll() {
     var sections = document.querySelectorAll('.sectionSelector');
+    
     for (var i = 0; i < sections.length; i++) {
-      if (isElementInViewport(sections[i])) {
+        var windowheight = window.innerHeight;
+        var rectTop = sections[i].getBoundingClientRect().top;
+        var rectpoint = 150;
+
+      if (rectTop < windowheight - rectpoint) {
         sections[i].classList.add('show');
       } else {
         sections[i].classList.remove('show');
