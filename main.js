7abbcbd05aca97cfe905e43b50569bda3eec4b20
Kevin <ksully121888@gmail.com>
2023-10-30 11:52:47 -0400

    Cleaned up a bit of language

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-// function isElementInViewport(el) {
-//     var rect = el.getBoundingClientRect();
-//     // return (
-//     //   rect.top >= 0 &&
-//     //   rect.left >= 0 &&
-//     //   rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//     //   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     // );
-//     return (
-        
 
-//     )
-//   }
   
   function handleScroll() {
     var sections = document.querySelectorAll('.sectionSelector');
@@ -28,4 +16,22 @@
     }
   }
   
+  function handleSlide() {
+    var sections = document.querySelectorAll('.sliderSelector');
+    
+    for (var i = 0; i < sections.length; i++) {
+        var windowheight = window.innerHeight;
+        var rectTop = sections[i].getBoundingClientRect().top;
+        var rectpoint = 150;
+
+      if (rectTop < windowheight - rectpoint) {
+        sections[i].classList.add('show');
+      } else {
+        sections[i].classList.remove('show');
+      }
+    }
+  }
+
+
   window.addEventListener('scroll', handleScroll);
+  window.addEventListener('scroll', handleSlide);
